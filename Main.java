321627be55050d483e095193272e7efe444a8f78
vladgnes <Vhnes02@gmail.com>
2016-07-31 21:22:03 +0300

    no message

--- Main.java
+++ Main.java
@@ -10,8 +10,11 @@ public class Main {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int n = scanner.nextInt();
+        double result = 0;
+        OneSegmentCalculationThread [] oneSegmentCalculationThreads = new OneSegmentCalculationThread[n-1];
         for (int i = 0;i < n-1;i++){
-            OneSegmentCalculationThread oneSegmentCalculationThread = new OneSegmentCalculationThread(a,b,n,i);//(doubleFunction.apply(a + i) + doubleFunction.apply(a + i + 1))/2 * (a+i+i - a+i);
+            oneSegmentCalculationThreads[i] = new OneSegmentCalculationThread(a,b,n,i);//(doubleFunction.apply(a + i) + doubleFunction.apply(a + i + 1))/2 * (a+i+i - a+i);
+            result += oneSegmentCalculationThreads[i].sum;
         }
     }
 }
