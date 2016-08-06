1857b9994fe2f520eaeac1747d52d23dd6df6db7
vladgnes <Vhnes02@gmail.com>
2016-08-06 20:46:45 +0300

    change structure of program

--- Main.java
+++ Main.java
@@ -16,5 +16,6 @@ public class Main {
             oneSegmentCalculationThreads[i] = new OneSegmentCalculationThread(a,b,n,i);//(doubleFunction.apply(a + i) + doubleFunction.apply(a + i + 1))/2 * (a+i+i - a+i);
             result += oneSegmentCalculationThreads[i].sum;
         }
+        System.out.println(result);
     }
 }
--- OneSegmentCalculationThread.java
+++ OneSegmentCalculationThread.java
@@ -5,6 +5,7 @@ import java.util.function.DoubleFunction;
  */
 public class OneSegmentCalculationThread extends Thread {
     int a,b,n,i;
+    double var1,var2;
     double sum = 0;
     DoubleFunction<Double> doubleFunction = new DoubleFunction<Double>() {
         @Override
@@ -17,10 +18,14 @@ public class OneSegmentCalculationThread extends Thread {
         this.b = b;
         this.n = n;
         this.i = i;
+        var1 = doubleFunction.apply(a + i);
+        var2 =  doubleFunction.apply(a + i + 1);
+
+
         start();
     }
 
     public void run(){
-        sum = (doubleFunction.apply(a + i) + doubleFunction.apply(a + i + 1))/2 * (a+i+i - a+i);
+        sum = (var1 + var2)/2 * (a+i+i - a+i);
     }
 }
